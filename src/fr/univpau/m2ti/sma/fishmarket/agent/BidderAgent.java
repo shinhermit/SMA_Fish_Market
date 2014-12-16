7ff167eb1975fb39e793206d0529586800443a92
Josuah Aron <josuaharon@hotmail.com>
2014-12-16 12:53:16 +0100

    [DF look-up] Adding DF registration and look-up.

--- src/fr/univpau/m2ti/sma/fishmarket/agent/BidderAgent.java
+++ src/fr/univpau/m2ti/sma/fishmarket/agent/BidderAgent.java
@@ -1,10 +1,23 @@
 package fr.univpau.m2ti.sma.fishmarket.agent;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import jade.core.AID;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 
 @SuppressWarnings("serial")
 public class BidderAgent extends Agent
 {
+	private AID marketAgent;
+	
+	private static final Logger LOGGER =
+			Logger.getLogger(MarketAgent.class.getName());
+	
 	@Override
 	protected void setup()
 	{
@@ -16,4 +29,41 @@ public class BidderAgent extends Agent
 	{
 		// Un-suscribe auctions
 	}
+    
+    /**
+     * Retrives the agents (AIDs) with which this agent needs to communicate.
+     * Searches for TravelAgency and ClientView agents
+     */
+    private AID getMarketAgent()
+    {
+    	AID marketAgent = null;
+    	
+        DFAgentDescription marketTemplate = new DFAgentDescription();
+        ServiceDescription marketSd = new ServiceDescription();
+        
+        // Template for searching the market agent
+        marketSd.setType(MarketAgent.SERVICE_DESCIPTION);
+        marketTemplate.addServices(marketSd);
+        
+        try
+        {
+            DFAgentDescription[] marketResult = DFService.search(this, marketTemplate);
+            
+            if(marketResult.length == 1)
+            {
+            	marketAgent = marketResult[0].getName();
+            }
+            else
+            {
+            	BidderAgent.LOGGER.log(Level.SEVERE, null, "Logic error: multiple market agents found !");
+            }
+        }
+        catch (FIPAException fe)
+        {
+            System.err.println("CasomClient::_searchDF : DF lookup error. "+fe.getLocalizedMessage());
+            BidderAgent.LOGGER.log(Level.SEVERE, null, fe);
+        }
+        
+        return marketAgent;
+    }
 }
--- src/fr/univpau/m2ti/sma/fishmarket/agent/MarketAgent.java
+++ src/fr/univpau/m2ti/sma/fishmarket/agent/MarketAgent.java
@@ -1,21 +1,70 @@
 package fr.univpau.m2ti.sma.fishmarket.agent;
 
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @SuppressWarnings("serial")
 public class MarketAgent extends Agent
 {
-	public static final String SERVICE = "FISH_MARKET_SERVICE";
+	private static final Logger LOGGER =
+			Logger.getLogger(MarketAgent.class.getName());
+	
+	public static final String SERVICE_DESCIPTION = "FISH_MARKET_SERVICE";
 	
 	@Override
 	protected void setup()
 	{
 		// Register service to DF
+        try
+        {
+            _register2DF();
+        }
+        catch (FIPAException ex)
+        {
+            System.err.println("TravelAgency::setup : DF registration error. "+ex);
+            MarketAgent.LOGGER.log(Level.SEVERE, null, ex);
+        }
+        
+        // Add behaviors
 	}
 	
 	@Override
 	protected void takeDown()
 	{
 		// De-register service from DF
+        try
+        {
+            DFService.deregister(this);
+        }
+        catch (FIPAException fe)
+        {
+            System.err.println("TravelAgency::takeDown : DF de-registration error. "+fe);
+            MarketAgent.LOGGER.log(Level.SEVERE, null, fe);
+        }
+        
+        super.takeDown();
 	}
+    
+    /**
+     * Add a description of this agent's services to the DF.
+     * @throws FIPAException when an error occurs.
+     */
+    private void _register2DF()
+    		throws FIPAException
+    {
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType(MarketAgent.SERVICE_DESCIPTION);
+        sd.setName(this.getAID().getName());
+        
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(this.getAID());
+        
+        dfd.addServices(sd);
+        DFService.register(this, dfd);
+    }
 }
