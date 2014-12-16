cb39b73589b067bc0bd017f86dc1a4fac959fcfe
CotCotCoder <emilien.arino@gmail.com>
2014-12-16 16:54:59 +0100

    [Added seller states]

---
+++ src/fr/univpau/m2ti/sma/fishmarket/behaviour/seller/states/StateInitialOrAfterPriceIncreaseAnnounce.java
@@ -0,0 +1,20 @@
+package fr.univpau.m2ti.sma.fishmarket.behaviour.seller.states;
+
+import jade.core.behaviours.OneShotBehaviour;
+
+@SuppressWarnings("serial")
+public class StateInitialOrAfterPriceIncreaseAnnounce extends OneShotBehaviour {
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+		System.out.println("action => " + getBehaviourName());
+	}
+
+	@Override
+	public int onEnd()
+	{
+		System.out.println("onEnd => " + getBehaviourName());
+		return 0;
+	}
+}
