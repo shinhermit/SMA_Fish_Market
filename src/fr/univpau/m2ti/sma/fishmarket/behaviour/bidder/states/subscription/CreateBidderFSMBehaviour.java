0a06b8e50e30f2384a847d06cd7ce7e0885c6976
CotCotCoder <emilien.arino@gmail.com>
2015-01-21 12:12:05 +0100

    [CreateBidderFSMBehaviour]Fixed compile error

--- src/fr/univpau/m2ti/sma/fishmarket/behaviour/bidder/states/subscription/CreateBidderFSMBehaviour.java
+++ src/fr/univpau/m2ti/sma/fishmarket/behaviour/bidder/states/subscription/CreateBidderFSMBehaviour.java
@@ -4,6 +4,8 @@ import fr.univpau.m2ti.sma.fishmarket.agent.BidderAgent;
 import fr.univpau.m2ti.sma.fishmarket.behaviour.bidder.SubsribeToAuctionBehaviour;
 import jade.core.Agent;
 import jade.core.behaviours.OneShotBehaviour;
+
+import java.util.Random;
 import java.util.logging.Logger;
 
 /**
@@ -24,13 +26,47 @@ public class CreateBidderFSMBehaviour extends OneShotBehaviour
     }
 
     @Override
-    public void action() {
+    public void action()
+    {
         System.out.println("action => " + getBehaviourName());
 
         BidderAgent bidderAgent =
                 (BidderAgent) super.myAgent;
 
-        bidderAgent.createBidderFSM(this.myFSM.getLastSubscribedAuction());
+        bidderAgent.createBidderFSM(
+            this.myFSM.getLastSubscribedAuction(),
+            this.getNewPriceLimit(1000, 2000)
+        );
+    }
+
+    private long getNewPriceLimit(long min, long max)
+    {
+        Random rand = new Random();
+
+        if (min == Long.MAX_VALUE)
+        {
+            min = Long.MAX_VALUE - 1;
+        }
+        
+        if (max >= min)
+        {
+            max = min + 1;
+        }
+
+        long randLong = rand.nextLong();
+
+        if (randLong < min)
+        {
+            randLong = randLong + min;
+        }
+
+        if (randLong > max)
+        {
+            randLong = (randLong % (max - min)) + min;
+        }
+
+
+        return randLong;
     }
 
 }
